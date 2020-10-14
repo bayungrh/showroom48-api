6ef2920fc3cc971e45fd7f3de58ae16e15e4e448
MuhBayu <bnugraha00@gmail.com>
2020-10-14 23:55:06 +0700

    add promise map

--- api/rooms/index.ts
+++ api/rooms/index.ts
@@ -1,6 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 import request from 'unirest';
 import moment from 'moment';
+import Promise from 'bluebird';
 
 const groupPerGroup = (list, selected = '') => {
     let groups = ['AKB48', 'HKT48', 'JKT48', 'NGT48', 'NMB48', 'SKE48', 'STU48'];
@@ -19,35 +20,53 @@ const groupPerGroup = (list, selected = '') => {
     return isValid ? temp[selected] : temp;
 }
 
+const profile = (roomId) => {
+    return request.get(`https://www.showroom-live.com/api/room/profile?room_id=${roomId}`).then(data => {
+        const {
+            room_id, room_name, birthday, current_live_started_at, follower_num, genre_name, avatar, 
+            award_list, banner_list, is_birthday, is_official, is_onlive, league_label, room_level, main_name
+        } = data.body;
+        return  {
+            room_id, room_name, room_level, league_label, main_name, avatar, award_list,
+            birthday, banner_list, current_live_started_at, follower_num, genre_name, is_birthday, is_official, is_onlive
+        }
+    });
+}
+
 const roomList = (query) => {
-    const {
-        upcomingLive,
-        liveNow,
-        group,
-        roomId
-    } = query;
+    const { upcomingLive, liveNow, group, roomId } = query;
     const upcomingFilter = (list) => list.filter(i => i.next_live_schedule !== 0);
     const liveNowFilter = (list) => list.filter(i => i.is_live === true);
     const personFilter = (list, roomId) => list.filter(i => i.id == roomId);
 
-    return request.get('https://campaign.showroom-live.com/akb48_sr/data/room_status_list.json').then(async res => {
+    return request.get('https://campaign.showroom-live.com/akb48_sr/data/room_status_list.json').then(res => {
         let data = res.body;
 
         if(upcomingLive === "true") data = upcomingFilter(data);
         if(liveNow === "true") data = liveNowFilter(data);
         if(roomId) data = personFilter(data, roomId);
 
-        data = data.map(row => {
+        data = Promise.map(data, async row => {
             const next_live_schedule = row.next_live_schedule;
             row.next_live_schedule = next_live_schedule > 0 ? moment(next_live_schedule * 1000).format('MM/DD h:mm A~') : 0;
             row.next_live_schedule_2 = next_live_schedule > 0 ? moment(next_live_schedule * 1000).format('DD MMMM YYYY, HH:mm') : 0;
+            row.images = {
+                small: row.image_url.replace('_m.', '_s.'),
+                medium: row.image_url,
+                large: row.image_url.replace('_m.', '_l.')
+            }
+            row.url_show = `https://www.showroom-live.com/${row.url_key}`;
             row.ranking = `${process.env.BASE_URL}/api/rooms/ranking?roomId=${row.id}`;
+            row.detail = `${process.env.BASE_URL}/api/rooms/?roomId=${row.id}`;
+            if(roomId) row.profile = await profile(row.id);
+            delete row.image_url;
             return row;
-        }).sort((a, b) => {
+        }).then(data => data.sort((a, b) => {
             const textA = a.name.toUpperCase();
             const textB = b.name.toUpperCase();
             return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        })
+        }));
+
         if(!roomId) data = groupPerGroup(data, group);
 
         return (roomId && data.length > 0) ? data[0] : data;
